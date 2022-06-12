bc45d5cf1cb577e92acb84b538eb10bfd7e1b1d8
Ozbey <gokhanozbey@gmail.com>
2022-06-12 17:53:32 -0400

    added BaseEntity & updated Product entity

--- src/main/java/com/cydeo/model/Product.java
+++ src/main/java/com/cydeo/model/Product.java
@@ -2,26 +2,36 @@ package com.cydeo.model;
 
 import com.cydeo.enums.Status;
 import com.cydeo.enums.Unit;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 
 
-
+@Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
-public class Product {
+public class Product extends BaseEntity{
 
     private String name;
     private String category;
     private String description;
     private Long low_limit_alert;
+    @Enumerated(EnumType.STRING)
     private Unit unit;
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     //Quantity?
-    //Edit
+    //Edit?
 
+    //@OneToMany(mappedBy = "company_id",cascade = CascadeType.ALL)
+    //@JoinColumn(name = "invoice_product")
+    //private Invoice invoice;
+
+    //@OneToOne
+   //private StockDetails stockDetails;
+
+    //@OneToOne
+    //private Category category;
 }
