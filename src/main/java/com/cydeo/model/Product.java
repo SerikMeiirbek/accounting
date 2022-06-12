f7dbf4790d11fd3cc54828143e1b39e6e834e3ea
Ozbey <gokhanozbey@gmail.com>
2022-06-12 17:11:31 -0400

    Product entity, status & unit enums

--- src/main/java/com/cydeo/model/Product.java
+++ src/main/java/com/cydeo/model/Product.java
@@ -1,5 +1,27 @@
 package com.cydeo.model;
 
+import com.cydeo.enums.Status;
+import com.cydeo.enums.Unit;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Product {
+
+    private String name;
+    private String category;
+    private String description;
+    private Long low_limit_alert;
+    private Unit unit;
+    private Status status;
+
+    //Quantity?
+    //Edit
 
 }
