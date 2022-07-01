c3b4ef54228a19efe3d7d4c802f64282a7f4b3ae
Ozbey <gokhanozbey@gmail.com>
2022-07-01 11:44:48 -0400

    CategoryDTO

---
+++ src/main/java/com/cydeo/dto/CategoryDTO.java
@@ -0,0 +1,25 @@
+package com.cydeo.dto;
+
+
+import com.cydeo.model.Company;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CategoryDTO {
+
+    @NotBlank
+    private String description;
+
+    private boolean enabled;
+
+    @NotNull
+    private Company company;
+
+}
