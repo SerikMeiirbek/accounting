dc5d8cefb4067aa4a2615649df6f5e835099794d
Ozbey <gokhanozbey@gmail.com>
2022-07-01 12:17:05 -0400

    ProductDTO

---
+++ src/main/java/com/cydeo/dto/ProductDTO.java
@@ -0,0 +1,51 @@
+package com.cydeo.dto;
+
+
+import com.cydeo.enums.Status;
+import com.cydeo.enums.Unit;
+import com.cydeo.model.Category;
+import com.cydeo.model.Company;
+import com.cydeo.model.Invoice;
+import com.cydeo.model.StockDetails;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductDTO {
+
+
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String name;
+
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String description;
+
+    @NotBlank // @NotBlank or @NotNull?
+    private Long low_limit_alert;
+
+    private Unit unit;
+    private Status status;
+
+    @NotNull
+    private Company company;
+
+    @NotNull
+    private Category category;
+
+    @NotNull
+    private Invoice invoice; //Not sure if needs to be here
+
+    @NotNull
+    private StockDetails stockDetails; //Not sure the location
+
+
+}
--- src/main/java/com/cydeo/model/Company.java
+++ src/main/java/com/cydeo/model/Company.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Entity
 @NoArgsConstructor
 @Data
 @Table(name = "company")
