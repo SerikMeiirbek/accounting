da07870daecd56c94617aa53d676f83a04673e91
Ozbey <gokhanozbey@gmail.com>
2022-07-09 15:27:06 -0400

    CategoryServiceImpl raw implementation

---
+++ src/main/java/com/cydeo/service/serviceImp/CategoryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cydeo.service.serviceImp;
+
+import com.cydeo.dto.CategoryDTO;
+import com.cydeo.service.CategoryService;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public List<CategoryDTO> getAllCategories() {
+        return null;
+    }
+}
