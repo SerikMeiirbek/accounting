f46ceb7fdca536afd9f07edb9ce7cff22f125eaf
Ozbey <gokhanozbey@gmail.com>
2022-07-09 15:09:28 -0400

    Product,Category,Payment Service classes

---
+++ src/main/java/com/cydeo/service/PaymentService.java
@@ -0,0 +1,26 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.PaymentDTO;
+import com.cydeo.enums.Month;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PaymentService {
+
+    List<PaymentDTO> getAllPayments(); //??? might filter
+
+    PaymentDTO findByMonth(Month month);
+
+    PaymentDTO findByYear(LocalDate localDate);
+
+    PaymentDTO fidByAmount(BigDecimal amount);
+
+    PaymentDTO findByInstitutionId();
+
+   PaymentDTO createPayment(PaymentDTO paymentDTO);//???
+
+      //Update no payment id?
+      //Delete
+}
