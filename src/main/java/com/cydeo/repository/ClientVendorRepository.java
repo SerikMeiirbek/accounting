19c2774f69e7224bd305663ec8cf10048c619dfd
n7owhad <n7owhad@hotmail.com>
2022-07-18 01:43:41 -0400

    7/18 fixes for Controller, ServiceImp

--- src/main/java/com/cydeo/repository/ClientVendorRepository.java
+++ src/main/java/com/cydeo/repository/ClientVendorRepository.java
@@ -3,9 +3,10 @@ package com.cydeo.repository;
 import com.cydeo.dto.ClientVendorDTO;
 import com.cydeo.model.ClientVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ClientVendorRepository extends JpaRepository<ClientVendor,Long> {
 
 }
